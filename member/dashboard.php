f6d51ca75891f6f31be22c38682d7d657d89ace1
John Alek Nacaytuna <nacaytunajohnalek@gmail.com>
2025-05-03 21:14:51 +0800

    Started members' dashboard

---
+++ member/dashboard.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles.css">
+    <title>BeastModeHQ</title>
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">BeastModeHQ</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Pricing</a>
+                </div>
+
+
+            </div>
+
+        </div>
+    </nav>
+
+    
+    <h1 class="mt-5 ms-5">Welcome back, Jose!</h1>
+
+    <!-- main -->
+    <main class="container mt-5">
+       
+        <div class="row justify-content-evenly">
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Upcoming Classes</p>
+                <h3 class="text-center text-white">12</h3>
+            </div>
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Sessions this Month</p>
+                <h3 class="text-center text-white">20</h3>
+            </div>
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Attendance Rate</p>
+                <h3 class="text-center text-white">80%</h3>
+            </div>
+        </div>
+
+        <h2 class="mt-5">Available Classes</h2>
+
+        <div class="row justify-content-evenly mt-3">
+            <div class="col-3 bg-primary p-3">
+                <img class="d-block mx-auto" src="../images/workout.jpg" width="250px" height="250px" alt="black man working out">
+                <p class="text-white fw-semibold fs-4">Calisthenics Training</p>
+                <p class="text-white mt-n6 fw-lighter">Mon, April 29, 2025 - 10:00 AM</p>
+
+                <div class="row justify-content-between px-1">
+                    <p class=" col-4 text-white fw-lighter">5 spots left</p>
+                    <button type="submit" class=" col-4 btn btn-secondary ms-5">Enroll</button>
+                </div>
+                
+            </div>
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Sessions this Month</p>
+                <h3 class="text-center text-white">20</h3>
+            </div>
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Attendance Rate</p>
+                <h3 class="text-center text-white">80%</h3>
+            </div>
+        </div>
+
+        <div class="row justify-content-evenly mt-3">
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Upcoming Classes</p>
+                <h3 class="text-center text-white">12</h3>
+            </div>
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Sessions this Month</p>
+                <h3 class="text-center text-white">20</h3>
+            </div>
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Attendance Rate</p>
+                <h3 class="text-center text-white">80%</h3>
+            </div>
+        </div>
+
+        <div class="row justify-content-evenly mt-3">
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Upcoming Classes</p>
+                <h3 class="text-center text-white">12</h3>
+            </div>
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Sessions this Month</p>
+                <h3 class="text-center text-white">20</h3>
+            </div>
+            <div class="col-3 bg-primary p-3">
+                <p class="text-white">Attendance Rate</p>
+                <h3 class="text-center text-white">80%</h3>
+            </div>
+        </div>
+
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
